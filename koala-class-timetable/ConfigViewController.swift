cebe27d8223cb672625182da74071b96a8ac2432
ShoTada <shownfy05@gmail.com>
2017-03-29 14:33:22 +0900

    [add] TabBarによって切り替えのできる時間割画面と設定画面を追加

---
+++ koala-class-timetable/ConfigViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ConfigViewController.swift
+//  koala-class-timetable
+//
+//  Created by TDS on 2017/03/29.
+//  Copyright © 2017年 tadasho. All rights reserved.
+//
+
+import UIKit
+
+class ConfigViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
